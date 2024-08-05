8d220aed76f1a0d0e0564fafb17f43b3fa1506d0
Sandipan03 <sandipan2018@yahoo.com>
2024-08-05 14:39:30 +0530

    commit2

--- components/Text.js
+++ components/Text.js
@@ -7,7 +7,7 @@ import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 
-function text({className}) {
+function Text({className}) {
     
     useGSAP(()=>{
        gsap.to("*",{
@@ -83,4 +83,4 @@ function text({className}) {
   )
 }
 
-export default text
+export default Text
